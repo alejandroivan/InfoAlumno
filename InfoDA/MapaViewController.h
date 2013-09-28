05ddfa9eadca64ca802a81cf6d790cf08a5aa59e
Alejandro Iván Melo Domínguez <alejandroivan@ingenieros.com>
2013-09-27 22:34:35 -0300

    Versión 1.2.5 (última versión antes de cerrar el proyecto)
    - Corrección de errores
    - Agregado mapa de la Universidad
    - La barra de navegación ahora se muestra con el color correcto en iOS 7
    
    Signed-off-by: Alejandro Iván Melo Domínguez <alejandroivan@ingenieros.com>

---
+++ InfoDA/MapaViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapaViewController.h
+//  InfoAlumno
+//
+//  Created by Alejandro Iván Melo Domínguez on 24-09-13.
+//  Copyright (c) 2013 iOS Devel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapaViewController : UIViewController
+
+@end
---
+++ InfoDA/MapaViewController.m
@@ -0,0 +1,38 @@
+//
+//  MapaViewController.m
+//  InfoAlumno
+//
+//  Created by Alejandro Iván Melo Domínguez on 24-09-13.
+//  Copyright (c) 2013 iOS Devel. All rights reserved.
+//
+
+#import "MapaViewController.h"
+
+@interface MapaViewController ()
+
+@end
+
+@implementation MapaViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
